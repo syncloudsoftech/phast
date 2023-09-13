78a0422265ecb8297abec8b67ed3bdc6fa76322a
Vaibhav Pandey <contact@vaibhavpandey.com>
2023-09-13 09:48:05 +0530

    Include alpinejs & bootstrap.

--- templates/_main.php
+++ templates/_main.php
@@ -1,8 +1,16 @@
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $this->e($title) ?></title>
+    <link href="//unpkg.com/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//unpkg.com/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
-<?= $this->section('content') ?>
+<div class="container my-3">
+    <?= $this->section('content') ?>
+</div>
+<script src="//unpkg.com/alpinejs" defer></script>
+<script src="//unpkg.com/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- templates/home.php
+++ templates/home.php
@@ -1,4 +1,17 @@
 <?php $this->layout('_main', ['title' => 'Home']) ?>
 
-<h1>Home</h1>
-<p>Hello, <?= $this->e($name) ?></p>
+<h1 class="mb-3">Home</h1>
+<div x-data="{ name: '<?= $this->e($name) ?>' }">
+    <div class="row mb-3">
+        <div class="col-md-8 col-lg-6 col-xl-4">
+            <div class="input-group">
+                <span class="input-group-text">
+                    <i class="bi bi-person"></i>
+                </span>
+                <!--suppress HtmlFormInputWithoutLabel -->
+                <input type="text" class="form-control" placeholder="Enter your name" x-model="name">
+            </div>
+        </div>
+    </div>
+    <p class="mb-0">Hey, <span x-text="name"></span> I hope you are feeling good today.</p>
+</div>
